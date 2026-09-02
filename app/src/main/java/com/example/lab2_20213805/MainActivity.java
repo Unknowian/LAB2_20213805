f60e2dff089b18281793b519993198d6838498ea
jeniffer <a20223291@pucp.edu.pe>
2026-09-01 20:10:45 -0500

    2do

--- app/src/main/java/com/example/lab2_20213805/MainActivity.java
+++ app/src/main/java/com/example/lab2_20213805/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.lab2_20213805;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,4 +27,49 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+/*
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main_act,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int itemId = item.getItemId();
+        if (itemId == R.id.wifi) {
+            Toast.makeText(this, "btn wifi presionado", Toast.LENGTH_LONG).show();
+        } else if (itemId == R.id.add) {
+            Toast.makeText(this, "btn add presionado", Toast.LENGTH_LONG).show();
+        } else if (itemId == R.id.notify) {
+            //Toast.makeText(this, "btn notify presionado", Toast.LENGTH_SHORT).show();
+            Log.d ("msgOptAppBar", "App Bar onclik");
+            View menuItemView = findViewById(R.id.notify);
+
+
+            PopupMenu popupMenu =  new PopupMenu(this, menuItemView);
+            popupMenu.getMenuInflater().inflate(R.menu.menu_popup, popupMenu.getMenu());
+            popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem item) {
+                    if (item.getItemId() == R.id.reply_all) {
+                        Log.d ("msgPopup", "replyAll");
+                        return true;
+                    } else if (item.getItemId() == R.id.forward) {
+                        Log.d ("msgPopup", "forward");
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+            });
+            popupMenu.show();
+
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }*/
+
 }
